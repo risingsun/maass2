3e4a40759cdf0f0dcc35ff5dfa6586d257348f88
kirtiparihar <pariharkirti24@gmail.com>
2011-02-18 18:58:09 +0530

    send Direct message

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,3 @@
 class AccountsController < ApplicationController
-
 end
 
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,8 +7,15 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
+  def create
+    debugger
+    @message=Message.create(params[:profile])
+  end
+
   def direct_message
+    @profile=current_user.profile
     @message = Message.new
-    render "new"
+    @to_list = [Profile.find(params[:profile_id])]
+    render :action => "new"
   end
 end
