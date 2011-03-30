83d9f4823d124fe0785447f215980666a3e219b5
kirtiparihar <pariharkirti24@gmail.com>
2011-03-30 14:41:02 +0530

    Create Multiple Forum Topics In Forum

--- app/controllers/forum_topics_controller.rb
+++ app/controllers/forum_topics_controller.rb
@@ -1,2 +1,33 @@
 class ForumTopicsController < ApplicationController
+
+  before_filter :hide_side_panels
+
+  def index
+    @forum = Forum.find(params[:id])
+    @forum_topics = @forum.forum_topics
+  end
+
+  def new
+    @forum = Forum.find(params[:forum_id])
+    @forum_topic = @forum.forum_topics.new
+  end
+
+  def create
+    @forum = Forum.find(params[:forum_id])
+    @forum_topic = @forum.forum_topics.build(params[:forum_topic])
+    @forum_topic.save
+    redirect_to forums_path()
+  end
+
+#  def destroy
+#    @forum.forum_topics.destroy
+#    flash[:notice] = "Successfully destroyed forum."
+#    redirect_to forums_path
+#  end
+  private
+
+  def hide_side_panels
+    @hide_panels = true
+  end
+
 end
--- app/models/forum.rb
+++ app/models/forum.rb
@@ -1,3 +1,4 @@
 class Forum < ActiveRecord::Base
   validates :name, :presence => true
+  has_many :forum_topics
 end
