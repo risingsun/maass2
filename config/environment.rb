daaed68d0a940cfef23cd352178d067afac112fa
kirtiparihar <pariharkirti24@gmail.com>
2011-04-12 16:22:36 +0530

    signin MAASS with yahoo id

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,5 @@
 require File.expand_path('../application', __FILE__)
 require 'openssl'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-# Initialize the rails application
 Maass2::Application.initialize!
+
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,6 @@
    provider :twitter, 'uqb5tjFy0PhKut5RQCUg', 'wXEsnXE2EMJoL5YVVZQcvyAHGiOOSZVSyy8XpPnDYs'
    provider :facebook, '0afea3ef43845e21ff2d3c991954b3d2', '06d707dca86feddf20e85b68d639e6c6'
    provider :linked_in, 'cWKTofwhrB16xzPxOTWsQ6DbImXT2UV-lzUctxoGmAr1D4UlHsG8XV7nGUNv_j_8', 'eS8qNy0prPL9iEDD2Y9PidcjnQSCPAD-Z0uPCExeVVybrogJOroDfbUB-hxP50m1'
-end
+#   provider :google_apps, OpenID::Store::Filesystem.new('/tmp')
+   provider :OpenID, OpenID::Store::Filesystem.new('/tmp'), :name => 'yahoo', :identifier => 'yahoo.com'
+ end
