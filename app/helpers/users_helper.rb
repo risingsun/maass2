f43782ec37ca08da249cfff743b7ea861b8c68b2
kirtiparihar <pariharkirti24@gmail.com>
2011-04-19 12:12:12 +0530

    set personal info work info & education info CSS

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
-  def show_field(field_value,field_name,field)
-    if !field_value.blank? && @profile.can_see_field(field, @p)
-      "<tr><th align='right'>#{field_name} </th><td>#{field_value}</td></tr>" 
-    else
-      ""
-    end
-  end
+#  def show_field(field_value,field_name,field)
+#    if !field_value.blank? && @profile.can_see_field(field, @p)
+#      "<tr><th align='right'>#{field_name} </th><td>#{field_value}</td></tr>"
+#    else
+#      ""
+#    end
+#  end
+
+  
 end
