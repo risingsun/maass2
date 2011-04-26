d3050d38a24f88dbe54e153315635155627318fc
Amit Gupta <amitg3112@gmail.com>
2011-04-26 20:25:15 +0530

    applied jquery for poll

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -8,7 +8,9 @@ class VotesController < ApplicationController
       @vote = @profile.poll_responses.new(:poll_option => @option, :poll => @option.poll)
       @vote.save
       respond_to do |format|
-        format.js
+        format.js do
+          render :partial => 'polls/poll_response', :locals => {:poll => @vote.poll}
+        end
       end
     end
   end
