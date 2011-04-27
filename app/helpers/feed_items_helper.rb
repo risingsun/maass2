48cefe2e10c00713dcc4e44358cffc7eb4ac3a92
Amit Gupta <amitg3112@gmail.com>
2011-04-27 19:29:27 +0530

    applied jquery on recent activity deletion

--- app/helpers/feed_items_helper.rb
+++ app/helpers/feed_items_helper.rb
@@ -8,11 +8,11 @@ module FeedItemsHelper
     parent = comment.commentable
     case parent.class.name
     when 'Profile'
-      "<strong>#{link_to_if in_html, comment.profile.full_name, comment.profile} wrote a comment on #{link_to_if in_html, parent.full_name+'\'s', profile_path(parent)} wall </strong>"
+      "<strong>#{link_to_if in_html, comment.profile.full_name, comment.profile} wrote a comment on #{link_to_if in_html, parent.full_name+'\'s', profile_path(parent)} wall </strong>".html_safe
     when 'Blog'
-      "<strong>#{link_to_if in_html, comment.profile.full_name, comment.profile} commented on #{link_to_if in_html, h(parent.title), profile_blog_path(parent.profile, parent)} </strong>"
+      "<strong>#{link_to_if in_html, comment.profile.full_name, comment.profile} commented on #{link_to_if in_html, h(parent.title), profile_blog_path(parent.profile, parent)} </strong>".html_safe
     when 'Event'
-      "<strong>#{link_to_if in_html, comment.profile.full_name, comment.profile}</strong>"
+      "<strong>#{link_to_if in_html, comment.profile.full_name, comment.profile}</strong>".html_safe
     end
   end
   #safe_helper :commentable_text, :x_feed_link
