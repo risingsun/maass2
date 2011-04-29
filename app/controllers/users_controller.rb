b0eb1e77302aebb0ed85f0b988dc658dfd1a2945
Kaushal <kaushal@kaushal-desktop.(none)>
2011-04-29 11:23:54 +0530

    update user controller for change password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,10 @@ class UsersController < Devise::RegistrationsController
 
   layout "plain"
   def update
-    @profile =  current_user.profile
-    @permission =@profile.permissions || @profile.permissions.build
+    @profile =  @p
     if resource.update_with_password(params[:user])
       set_flash_message :notice, :updated
-      redirect_to edit_account_profile_path(current_user)
+      redirect_to edit_account_profile_path(@profile)
     else
       clean_up_passwords(current_user)
       render "profiles/edit_account"
