c188129fb22759f46e758b9f9feddd0c51cef227
Amit Gupta <amitg3112@gmail.com>
2011-05-05 20:26:45 +0530

    applied validations

--- app/models/poll_response.rb
+++ app/models/poll_response.rb
@@ -1,15 +1,12 @@
 class PollResponse < ActiveRecord::Base
+  
   belongs_to :profile
   belongs_to :poll_option
   belongs_to :poll
 
-  validates :profile, :presence => true
-  validates :poll_option, :presence => true
-  validates :poll, :presence => true
-  #validates_uniqueness_of :profile_id, :scope => :poll_id
+  validates :profile, :poll_option, :poll, :presence => true
 
-   after_save :update_poll_votes_count
-
+  after_save :update_poll_votes_count
 
   private
 
