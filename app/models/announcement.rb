f0edfd2917929c7bf273ad7cc867e0e594653508
kirtiparihar <pariharkirti24@gmail.com>
2011-05-13 16:33:35 +0530

    show & hide announcements

--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,4 +2,11 @@ class Announcement < ActiveRecord::Base
 
   validates :message, :starts_at, :ends_at, :presence => true
 
+  def self.current_announcements
+    all.select{|a| a.current_announcement?}
+  end
+
+  def current_announcement?
+    DateTime.current.between?(starts_at, ends_at)
+  end
 end
