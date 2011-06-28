d72ff0261fe31cff11234b882341ad038ba64ec6
Amit Gupta <amitg3112@gmail.com>
2011-06-28 18:11:57 +0530

    event model rspec testing

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,4 +32,5 @@ Maass2::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  config.active_record.observers = []
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,72 @@
 require 'spec_helper'
 
 describe Event do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:all) do
+    @event = Factory(:event)
+    @profile = Factory(:profile)
+    @profile_event = Factory(:profile_event, :profile=> @profile, :event=> @event)
+  end
+
+  let(:event) {@event.reload}
+  let(:profile_event) {@profile_event.reload}
+
+  it "should require title" do
+    no_title = Factory.build(:event, :title => "")
+    no_title.should_not be_valid
+  end
+
+  it "should require place" do
+    no_place = Factory.build(:event, :place => "")
+    no_place.should_not be_valid
+  end
+
+  it "should require start_date" do
+    no_start_date = Factory.build(:event, :start_date => "")
+    no_start_date.should_not be_valid
+  end
+
+  it "should require end_date" do
+    no_end_date = Factory.build(:event, :end_date => "")
+    no_end_date.should_not be_valid
+  end
+
+  it "should require description" do
+    no_description = Factory.build(:event, :description => "")
+    no_description.should_not be_valid
+  end
+
+  it "should check role_of_user" do
+    event.role_of_user(profile_event.profile).should_not be_blank
+    event.role_of_user(profile_event.profile).role.should == "Organizer"
+  end
+
+  it "should set_organizer" do
+    event.set_organizer(profile_event.profile).should_not be_blank
+    event.set_organizer(profile_event.profile).role.should == "Organizer"
+  end
+
+  it "should test method responded?" do
+    event.responded?(profile_event.profile).should be_true
+  end
+
+  it "should check method list" do
+    other_event = Factory(:event)
+    profile_event = Factory(:profile_event, :event => other_event, :role => "attending")
+    other_event.list(profile_event.role).first.should_not be_blank
+    other_event.list(profile_event.role).should be_kind_of(Array)
+  end
+
+  it "should test that user is_organizer?" do
+    event.is_organizer?(profile_event.profile).should be_true
+  end
+
+  it "should set_role_of_user" do
+    other_event = Factory(:event)
+    profile_event = Factory(:profile_event, :event => other_event, :role => "attending")
+    updated_role =  other_event.set_role_of_user(profile_event.profile, "not_attending")
+    updated_role.should_not be_blank
+    updated_role.role.should ==  "not_attending"
+  end
+  
 end
